88fcf890223d3f570959b36c8776c4b3b393ab24
AravindKKrishna <99062374+AravindKKrishna@users.noreply.github.com>
2024-03-29 17:53:22 +0530

    Update job-list.php

--- job-list.php
+++ job-list.php
@@ -532,7 +532,7 @@ $title = "Job List";
 									
 										<div class="footer-about-us">
 											<h5 class="footer-title">About Nightingale Jobs</h5>
-											<p>Nightingale Jobs is a job portal, online job management system developed by Nathaniel Nkrumah for his project in february 2018.</p>
+											<p>Nightingale Jobs is a job portal, online job management system developed by Aravind K Krishna and Ashkar r for his project in March023.</p>
 										
 										</div>
 
