131da74818e35122655808dd2fb4039712850050
AravindKKrishna <99062374+AravindKKrishna@users.noreply.github.com>
2024-03-29 18:01:37 +0530

    Update job-list.php

--- job-list.php
+++ job-list.php
@@ -587,7 +587,7 @@ $title = "Job List";
 							<div class="col-sm-4 col-md-4">
 							
 								<ul class="bottom-footer-menu">
-									<li><a >Developed by Nathaniel Nkrumah</a></li>
+								<li><a >Developed Aravind and Ashkar</a></li>
 								</ul>
 							
 							</div>
